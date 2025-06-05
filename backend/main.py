e55290f9269c703783fee996af5cefb3f98c5675
Priyanshu Singhal <150108714+priyanshusinghal12@users.noreply.github.com>
2025-06-05 07:31:10 -0400

    v2

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ bot = FootprintsBot()
 # Allow frontend requests from localhost
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
